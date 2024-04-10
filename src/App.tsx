5eb19fc3c6be6c58aca0aa45e8ac3c4365f87430
gaston pedraza <gaston08pedraza@gmail.com>
2024-04-10 15:52:54 -0300

    add images to cube

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,9 @@
+import RubikCube from "./components/RubikCube";
+
 export default function App() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <button class="btn btn-primary">Button</button>
-      <div className="mockup-phone">
-        <div className="camera"></div>
-        <div className="display">
-          <div className="artboard artboard-demo phone-1">Hi.</div>
-        </div>
-      </div>
+    <div id="app" className="container-fluid mx-auto">
+      <RubikCube />
     </div>
   );
 }
